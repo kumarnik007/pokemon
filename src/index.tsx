56e041cd91f93ffa930f329d47e6a34b5e316c7d
Nikhil Kumar <nkhlneil@gmail.com>
2023-01-02 12:36:27 +0100

    Initial commit : added the skeleton for the project

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(
+    <h1>Hello React!</h1>,
+  );
