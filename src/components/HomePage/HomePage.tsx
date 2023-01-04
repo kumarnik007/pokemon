c08699ca376fa527039b12ab4ef306f8fe534cfc
Nikhil Kumar <nkhlneil@gmail.com>
2023-01-04 16:01:33 +0100

    Updated description in the home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 export const HomePage = () => (
-  <h1 className="title">
-    Home Page
-  </h1>
+  <>
+    <h1 className="title">
+      Pokemon Home
+    </h1>
+
+    <p>
+      Welcome to this application!<br/>
+
+      Get a list of all pokemons by visting the <strong>Pokemons</strong> page from the above navigation bar.<br/>
+      Try searching for a pokemon by visiting the <strong>Search</strong> page from the above navigation bar.<br/>
+    </p>
+  </>
 );
